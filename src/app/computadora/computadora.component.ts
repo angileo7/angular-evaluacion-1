4c4dee0d97d1e68e6d5833eb7c861326a9a48db1
A1989 <a1989@A1989s-MacBook-Pro.local>
2024-08-25 00:58:12 -0600

    agregar computadora componente

---
+++ src/app/computadora/computadora.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, HostBinding } from '@angular/core';
+import { Computadora } from '../models/computadora';
+
+
+@Component({
+  selector: 'app-computadora',
+  standalone: true,
+  imports: [],
+  templateUrl: './computadora.component.html',
+  styleUrl: './computadora.component.css'
+})
+export class ComputadoraComponent {
+  @Input() computadora: Computadora;
+  @HostBinding('attr.class') cssClass = 'col-md-4';
+
+  constructor(){
+    
+  }
+}
---
+++ src/app/models/computadora.ts
@@ -0,0 +1,9 @@
+export class Computadora {
+    nombre:string;
+    ram:number;
+
+    constructor(a:string, b:number){
+        this.nombre = a;
+        this.ram = b;
+    }
+}
